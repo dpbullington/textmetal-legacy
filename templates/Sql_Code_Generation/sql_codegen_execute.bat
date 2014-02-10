6cb8ee36fa19ed9df2723a40ce443775f0b486d6
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2014-02-09 22:47:02 -0600

    Changes to support not allowing hash as valid variable name. Changes to support new database proxy templates.

--- templates/Sql_Code_Generation/sql_codegen_execute.bat
+++ templates/Sql_Code_Generation/sql_codegen_execute.bat
@@ -10,6 +10,7 @@ set PACKAGE_DIR_EXISTS=%PACKAGE_DIR%\nul
 
 set HISTORY_SCHEMA_NAME=history
 set ADO_NET_CONNECTION_STRING=Data Source=(local);User ID=TextMetalWebHostSampleLogin;Password=LrJGmP6UfW8TEp7x3wWhECUYULE6zzMcWQ03R6UxeB4xzVmnq5S4Lx0vApegZVH;Initial Catalog=TextMetalWebHostSample
+set DATA_OBFUSCATION_PROXY_CATALOG_NAME=TextMetalWebHostSample_Proxy
 
 :pkgDir
 
@@ -39,6 +40,7 @@ echo *** sql_codegen_execute ***
 	-sourcestrategy:"TextMetal.Framework.SourceModel.DatabaseSchema.Sql.SqlSchemaSourceStrategy, TextMetal.Framework.SourceModel" ^
 	-strict:"true" ^
 	-property:"HistorySchemaName=%HISTORY_SCHEMA_NAME%" ^
+	-property:"DataObfuscationProxyCatalogName=%DATA_OBFUSCATION_PROXY_CATALOG_NAME%" ^
 	-property:"ConnectionType=System.Data.SqlClient.SqlConnection, System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" ^
 	-property:"DataSourceTag=net.sqlserver"
 IF %ERRORLEVEL% NEQ 0 goto pkgError
