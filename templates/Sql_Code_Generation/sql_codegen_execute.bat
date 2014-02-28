529e4a239bec4c47a9b7a2d2a1b7552f15e6d7c3
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2014-02-28 00:07:13 -0600

    Fully baked hash substitution obfuscation.

--- templates/Sql_Code_Generation/sql_codegen_execute.bat
+++ templates/Sql_Code_Generation/sql_codegen_execute.bat
@@ -11,6 +11,7 @@ set PACKAGE_DIR_EXISTS=%PACKAGE_DIR%\nul
 set HISTORY_SCHEMA_NAME=history
 set ADO_NET_CONNECTION_STRING=Data Source=(local);User ID=TextMetalWebHostSampleLogin;Password=LrJGmP6UfW8TEp7x3wWhECUYULE6zzMcWQ03R6UxeB4xzVmnq5S4Lx0vApegZVH;Initial Catalog=TextMetalWebHostSample
 set DATA_OBFUSCATION_PROXY_CATALOG_NAME=TextMetalWebHostSample_Proxy
+set DATA_OBFUSCATION_TARGET_SERVER_NAME=FRAMEWORK
 
 :pkgDir
 
@@ -41,6 +42,7 @@ echo *** sql_codegen_execute ***
 	-strict:"true" ^
 	-property:"HistorySchemaName=%HISTORY_SCHEMA_NAME%" ^
 	-property:"DataObfuscationProxyCatalogName=%DATA_OBFUSCATION_PROXY_CATALOG_NAME%" ^
+	-property:"DataObfuscationTargetServerName=%DATA_OBFUSCATION_TARGET_SERVER_NAME%" ^
 	-property:"ConnectionType=System.Data.SqlClient.SqlConnection, System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" ^
 	-property:"DataSourceTag=net.sqlserver"
 IF %ERRORLEVEL% NEQ 0 goto pkgError
